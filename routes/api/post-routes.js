ed61f3a62070e558a94de1106d1715d139842eb1
Joe Flanagan <jflanagan@nieceequipment.local>
2020-08-13 15:52:28 -0500

    upvote not working, but no error messages

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
 // GET ALL POSTS
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [['created_at', 'DESC']], 
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'] ],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
@@ -26,7 +27,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'] ],
         include: [
             {
                 model: User,
@@ -86,6 +87,17 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+//PUT AN UPVOTE
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 
 //DELETE A POST
 router.delete('/:id', (req, res) => {
