dfd0f4901fe8164573e39dfd43179f3d004f9e72
josephptflanagan <josephptflanagan@gmail.com>
2020-08-19 00:25:24 -0500

    unexplainably broken...literally no idea how or why

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,11 +1,10 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('textarea[name="post-title"]').value.trim();
+    const title = document.querySelector('input[name="post-title"]').value.trim();
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
@@ -21,7 +20,6 @@ async function editFormHandler(event) {
     } else {
         alert(response.statusText);
     }
-
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
