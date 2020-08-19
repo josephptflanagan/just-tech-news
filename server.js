79e3444424bd8be50c902b88d8c9164c4fdeca06
josephptflanagan <josephptflanagan@gmail.com>
2020-08-18 21:27:49 -0500

    login and logout added

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const path = require('path');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 //server and port setup
 const app = express();
@@ -16,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
